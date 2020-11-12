3138244396a9dffca9faeab414dc74883739056e
User <user@Users-MacBook-Pro.local>
2020-11-12 18:32:29 +0200

    mil4

--- .github/index.js
+++ .github/index.js
@@ -20,7 +20,7 @@ async function resultsLoad(event){
         let result = await response.json();
         for(i=0; i<result.length; i++) {
             searchResults.innerHTML += `<li class="resultq__item">
-            <a href='./company'><img src='https://financialmodelingprep.com/image-stock/${result[i].symbol}.png' onerror="this.onerror=null;this.src='https://financialmodelingprep.com/image-stock/PEER.jpg'">${result[i].name}(${result[i].symbol})</a>
+            <a href='company.html?symbol=${result[i].symbol}'><img src='https://financialmodelingprep.com/image-stock/${result[i].symbol}.png' onerror="this.onerror=null;this.src='https://financialmodelingprep.com/image-stock/PEER.jpg'">${result[i].name}(${result[i].symbol})</a>
             <li>`
         }
     }else {
