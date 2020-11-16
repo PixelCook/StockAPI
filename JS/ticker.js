a8cbaf17538163ed8bb9704f046b938bbffbfb49
User <user@Users-MacBook-Pro.local>
2020-11-16 11:22:10 +0200

    initial

--- JS/ticker.js
+++ JS/ticker.js
@@ -1,5 +1,14 @@
-const marquee= document.getElementById('marquee');
-// MARQUEE
+const totallyunique = document.getElementById('marqueediv')
+// MARQUEE CONTAINER
+
+class divLoad {
+    divLoad(){
+    totallyunique.innerHTML = `<div class="marquee__inner" aria-hidden="true">
+    <span class="marquee__text" id="marquee"> </span>
+  </div>`
+}
+}
+
 
 class tickerValues {
     constructor(ticker, companyName, price, changesPercentage){
@@ -19,19 +28,24 @@ class tickerValues {
     }
 }
 
-async function tickerLoad(){
+async function tickerLoad(element){
     try{
         let fetched = await fetch(`https://financialmodelingprep.com/api/v3/gainers?apikey=38f0db76372c4155ed8a5fd5e0b70efd`)
         let resulted = await fetched.json();
             for(i=0; i<resulted.length; i++){
                 const ticker = new tickerValues(resulted[i].ticker, resulted[i].companyName, resulted[i].price, resulted[i].changesPercentage);
 
-                ticker.changeHtml(marquee);
+                ticker.changeHtml(element);
             }
 
     }catch(error){
         console.log(error)
     }
 }
-document.addEventListener('DOMContentLoaded', tickerLoad());
+document.addEventListener('DOMContentLoaded', () => {
+    const div = new divLoad
+    div.divLoad();
+    const marquee = document.getElementById('marquee');
+    tickerLoad(marquee)
+});
 // INTIALIZATION OF GETPAGE() ON DOM LOAD
