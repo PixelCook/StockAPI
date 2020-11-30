c81227960c16b01e6c366dd6b070628f6b601df1
User <user@Users-MacBook-Pro.local>
2020-11-30 13:30:11 +0200

    finalfinal

--- JS/ticker.js
+++ JS/ticker.js
@@ -26,30 +26,8 @@ class tickerValues {
     tag.innerHTML += ` ${this.ticker} ${this.companyName} $${this.price} <i class="${innerclass}">${this.changesPercentage} </i> --`;
   }
 }
-
-// async function tickerLoad(element) {
-//   try {
-//     let fetched = await fetch(
-//       `https://financialmodelingprep.com/api/v3/gainers?apikey=38f0db76372c4155ed8a5fd5e0b70efd`
-//     );
-//     let resulted = await fetched.json();
-//     for (i = 0; i < resulted.length; i++) {
-//       const ticker = new tickerValues(
-//         resulted[i].ticker,
-//         resulted[i].companyName,
-//         resulted[i].price,
-//         resulted[i].changesPercentage
-//       );
-//       ticker.changeHtml(element);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
 document.addEventListener("DOMContentLoaded", () => {
   const div = new divLoad();
   div.divLoad();
-  //   const marquee = document.getElementById("marquee");
-  //   tickerLoad(marquee);
 });
 // INTIALIZATION OF GETPAGE() ON DOM LOAD
